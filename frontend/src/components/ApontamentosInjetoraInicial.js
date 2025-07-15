b417cc622d7faa3ae7270cfdf700a969731dfac4
KrunitzkyCardoso <162230374+LucasCardoso255@users.noreply.github.com>
2025-07-15 15:04:32 -0300

    Update ApontamentosInjetoraInicial.js

--- frontend/src/components/ApontamentosInjetoraInicial.js
+++ frontend/src/components/ApontamentosInjetoraInicial.js
@@ -186,7 +186,7 @@ export default function ApontamentosInjetoraInicial() {
                             />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid item xs={12} sm={12} md={8}>
                             <Autocomplete
                                 options={funcionarios}
                                 getOptionLabel={(option) => option.nome_completo || ''}
@@ -199,7 +199,7 @@ export default function ApontamentosInjetoraInicial() {
                             />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid item xs={12} sm={12} md={8}>
                             <Autocomplete
                                 options={pecas}
                                 getOptionLabel={(option) => `${option.descricao_peca} (${option.codigo_peca})` || ''}
